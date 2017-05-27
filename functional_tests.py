71f320291a4d4d27791ef01a0709cec4762b02dd
Waqar Hocain <waqarmaxtan@gmail.com>
2017-05-26 21:25:49 -0400

    Write unittests for homepage

--- functional_tests.py
+++ functional_tests.py
@@ -8,6 +8,9 @@ class NewVisitorTest(unittest.TestCase):
     def setUp(self):
         self.browser = webdriver.Firefox()
 
+    def tearDown(self):
+        self.browser.quit()
+
     def test_can_shorten_a_url(self):
         # Mastan has heard about a new cool url-shortner. He check outs
         # its homepage
@@ -17,6 +20,21 @@ class NewVisitorTest(unittest.TestCase):
         self.assertIn("Shrtnr", self.browser.title)
         header_text = self.browser.find_element_by_tag_name("h1").text
         self.assertIn("Shrtnr", header_text)
+
+        # He is invited to enter url
+        input_box = self.browser.find_element_by_class_name("input_url")
+        self.assertEqual(input_box.get_attribute("placeholder"),
+                            "Enter your url here..")
+        # He enters https://www.github.com/waqarHocain in input box and press
+        # enter
+
+        # A JSON object is returned in response containing actual url
+        # https://www.github.com/waqarHocain and shortened url with
+        # <current_page_url>/1
+
+        # He enters the <current_page_url>/1 into browser address bar, he's
+        # redirected to https://github.com/waqarHocain
+
 
 
 if __name__ == "__main__":
--- url_shrtnr/tests.py
+++ url_shrtnr/tests.py
@@ -1,6 +1,21 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
+from django.core.urlresolvers import resolve
+from django.http import HttpRequest 
+from django.template.loader import render_to_string
 from django.test import TestCase
 
-# Create your tests here.
+from .views import homepage
+
+
+class HomepageTest(TestCase):
+
+    def test_root_url_resolves_to_homepage_view(self):
+        found = resolve("/")
+        self.assertEqual(found.func, homepage)
+
+    def test_homepage_returns_correct_html(self):
+        request = HttpRequest()
+        response = homepage(request)
+
+        expected_html = render_to_string("url_shrtnr/index.html")
+
+        self.assertEqual(response.content.decode(), expected_html)
